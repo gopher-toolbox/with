f2a9e8ac4286ff56eab0ad9b435d44729c62c2dd
Vasili Vasilyeu <vasili.v@tut.by>
2020-06-08 23:02:11 +0300

    Increase test coverage

--- temp_test.go
+++ temp_test.go
@@ -22,3 +22,18 @@ func TestTempCall(t *testing.T) {
 	)
 	assert.NoError(t, os.Remove(name))
 }
+
+func TestTempError(t *testing.T) {
+	var name string
+
+	assert.Error(t,
+		Temp("/the/directory/which/should/not/exist", "", func(f *os.File) error {
+			name = f.Name()
+			t.Logf("created file %q", name)
+			return nil
+		}),
+	)
+	if name != "" {
+		assert.NoError(t, os.Remove(name))
+	}
+}
